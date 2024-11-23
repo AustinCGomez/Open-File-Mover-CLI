17f57760795b3bb6583309e349bfedfdc5a5dfbd
Austin Gomez <austincgomez@gmail.com>
2024-11-23 02:15:58 -0600

    Update main.py
    Code review and refactoring progress for #70

--- main.py
+++ main.py
@@ -39,26 +39,19 @@ LICENSE = "Project published under the Unlicense License and dedicated to the pu
 
 
 
-'''The view class will contain all of our logic for what is visually seen to the user. '''
+''' The purpose of our view class is retrieve the input and output data that the user is requesting and then to be able to send it to our controller class for further analyzies'''
 class Views():
     def __init__(self, directory_from, directory_to):
-        self.dir_a = directory_from
-        self.dir_b = directory_to
+        self.directory_from = directory_from
+        self.directory_to = directory_to
         self.extension_list = []
-    # The main menu view will contain the structing for the first introduction that the user has to thew application.
 
-    def default_statements():
-        #Controller will send us a code that then invokes a default statement in one of our views.
-        print("HI")
 
-# The purpose of this function is to retrieve the directories from the user. 
     def retrieve_directories_view(self):
         print("A small window will open on your screen in the next few seconds which will allow you to pick your source directory.")
-        time.sleep(2)
         self.directory_from = filedialog.askdirectory()
         print("A small window will open on your screen in the next few seconds which will allow you to pick your destination directory.")
         self.directory_to = filedialog.askdirectory()
-        time.sleep(2)
         print("Source Directory:", self.directory_from)
         print("Destination Directory:", self.directory_to)
         click.echo("Are the chosen directories correct?")
@@ -69,13 +62,9 @@ class Views():
         )
 
         if action == "-y".lower():
-            print("Vertification complete...")
-            time.sleep(1)
             print("Initializing...")
-            time.sleep(1)
         elif action == "-n":
             print("We are going to reverify the direcotires as the next step.")
-
         return self.directory_from, self.directory_to
 
     def retrieve_just_one_directory_view(self):
@@ -120,12 +109,8 @@ class Views():
         print(GOODBYE)
         print(CONTRIBUTE)
         print(LICENSE)
-        print(AUTHOR)
         print("-" * 130)
         sys.exit()
-
-
-
 
     def move_files_output(self, fileName, dir_a, dir_b):
         output_padding = " " * (len("Name") - len("Extension"))
@@ -134,14 +119,52 @@ class Views():
         print(" ")
 
     def move_folders_output(self, dir_a, dir_b):
-        print(f"SUCCESS: The Folder has been moved from {self.dir_a} to {self.dir_b}")
+        print(f"SUCCESS: The Folder has been moved from {dir_a} to {dir_b}")
 
-
+''' The purpose of our controller class is to handle the logic that is sent from our view and then process what needs to be sent to the model to run specific programs.'''
 class Controller():
     def __init__(self, dir_a, dir_b, ExtList):
         self.dir_a = dir_a
-        self_dir_b = dir_b
+        self.dir_b = dir_b
         self.ExtList = ExtList
+
+    def view_instances(self, decision):
+
+        ViewChoices = Views(self.dir_a, self.dir_b)
+        # 1 = flag for ending program view.
+        if decision == 1:
+            print("Bro what")
+            ViewChoices.end_program_view()
+        # 2 = flag for retreiving the directories view.
+        elif decision == 2:
+            self.directory_value_a = None
+            self.directory_value_b = None
+            dir_a, dir_b = ViewChoices.retrieve_directories_view()
+            self.directory_value_a = dir_a
+            self.directory_value_b = dir_b
+            return self.directory_value_a, self.directory_value_b
+        # 3 = flag for retreve_extensions_view
+        elif decision == 3:
+            self.mylist = []
+            mylist = ViewChoices.retrieve_extensions_view(self.mylist)
+            self.mylist = mylist
+        elif decision == 4:
+            ViewChoices.retrieve_just_one_directory_view()
+            
+
+            
+    def model_instances(self, decision):
+
+        ModelChoices = Model(self.dir_a, self.dir_b, self.ExtList)
+        if decision == 1:
+            ModelChoices.move_2trash_command()
+        elif decision == 2:
+            ModelChoices.move_files_command()
+        elif decision == 3:
+            ModelChoices.move_folders_command()
+
+
+
     def command_line_options(self):
         StartView = Views(dir_a, dir_b)
         StartView.start_program_view()
@@ -162,29 +185,21 @@ class Controller():
         elif action == '-mrecycle':
             self.move_folder_2_trash()
         elif action == '-quit':
-            directoryA = None
-            directoryB = None
-            QuitProgram = Views(directoryA, directoryB)
-            QuitProgram.end_program_view()
+            self.view_instances(1)
+
 
     def move_files_by_extension(self):
-        self.directory_a = None 
-        self.directory_b = None
-        GetDirs = Views(self.directory_a, self.directory_b)
-        directory_a, directory_b = GetDirs.retrieve_directories_view()
-       
-        self.directory_a = directory_a  #Updating our instance variable from our view for the source directory.
-        self.directory_b = directory_b  #updating our instance variable from our view for the destination directory.
-
-
-        # Invoking another method at this stage in our view to gather a list of extensions. 
-        self.mylist = []
-        mylist = GetDirs.retrieve_extensions_view(self.mylist)
-        print("Let's check to see if mylist is populated", mylist)
-        self.mylist = mylist # We are now saving our list with the important file extensions.
-        # We will now ship our code to our model to handle the logic work for the operation and that information is going to be sent to our view. 
-        MoveFilesCommand = Model(self.directory_a, self.directory_b, self.mylist)
-        MoveFilesCommand.move_files_command()
+        directory_a = None
+        directory_b = None
+        #Retrieve the files by extension.
+        directory_a, directory_b = self.view_instances(2)
+        
+        self.dir_a = directory_a  #Updating our instance variable from our view for the source directory.
+        self.dir_b = directory_b  #updating our instance variable from our view for the destination directory.
+        print("dir_a is source", directory_a)
+        print("dir_b is destination", directory_b)
+        self.view_instances(3)
+        self.model_instances(2)
 
     # Move this to a function in the views eventually.
     def error_extensions(self):
@@ -192,36 +207,24 @@ class Controller():
         print(" ")
 
     def move_folders_1_by_1(self):
-        self.directory_a = None 
-        self.directory_b = None
-        GetDirs = Views(self.directory_a, self.directory_b)
-        directory_a, directory_b = GetDirs.retrieve_directories_view()
-       
-        self.directory_a = directory_a  #Updating our instance variable from our view for the source directory.
-        self.directory_b = directory_b  #updating our instance variable from our view for the destination directory.
-
-        # The next phase is to send the code over to our Model to process the logic aspect of the program.
-        self.mylist = []
-        MoveFoldersCommand = Model(self.directory_a, self.directory_b, self.mylist)
-        MoveFoldersCommand.move_folders_command()
-        SendBackController = Controller(self.directory_a, self.directory_b, self.ExtList)
-        SendBackController.command_line_options()
-
+        directory_a = None
+        directory_b = None
+        directory_a, directory_b = self.view_instances(2)
+        self.dir_a = directory_a  #Updating our instance variable from our view for the source directory.
+        self.dir_b = directory_b  #updating our instance variable from our view for the destination directory.
+        self.model_instances(3)
+    
     def move_folder_2_trash(self):
         self.directory_a = None
         self.directory_b = None
-        GetDirs = Views(self.directory_a, self.directory_b)
-        directory_to_trash = GetDirs.retrieve_just_one_directory_view()
-        self.directory_a = directory_to_trash
-        SendToTrash = Model(self.directory_a, self.directory_b, self.ExtList)
-        SendToTrash.move_2trash_command()
+        self.view_instances(3)
+        self.model_instances(1)
 
-    def error_exeption(self):
+    def relay_error_message(self):
         print("Fatal error has occured when attempting to move your folder to the trash. Please screenshot this and open a issue on our github.")
         print(" ")
         print("Sending you back to the main menu.. sorry about that. :( )")
         self.command_line_options()
-        time.sleep(1)
 
         
 
@@ -230,50 +233,80 @@ class Model():
         self.dir_a = dir_a
         self.dir_b = dir_b
         self.ExtList = ExtList
+    
+    def view_instances(self,decision, fileName):
+        InstanceView = Views(self.dir_a, self.dir_b)
+        if decision == 1:
+            InstanceView.move_files_output(self.dir_a, self.dir_b)
+        elif decision == 2: 
+            InstanceView.move_files_output(fileName, self.dir_a, self.dir_b)
+        elif decision ==3:
+            InstanceView.move_folders_output(fileName, self.dir_a, self.dir_b)
+
+
+    
+
+    def controller_instances(self, decision):
+        command_line_options = Controller(self.dir_a, self.dir_b, self.ExtList)
+        if decision == 1:
+            command_line_options.command_line_options()
+        elif decision == 2: 
+            command_line_options.relay_error_message()
+        elif decision == 3:
+            command_line_options.relay_error_message()
+        elif decision == 4:
+            command_line_options.error_extensions()
+
+    
+    
+    def call_foldermove_method(self):
+        MovedFolder = Views(self.dir_a, self.dir_b)
+        MovedFolder.move_folders_output(self.dir_a, self.dir_b)
+    
+    def filemoving_methods(self, decision, file):
+        #1 = We will instance View class for the move_files_output function.
+        FileMethods = Views(self.dir_a, self.dir_b)
+        if decision == 1:
+            # We are sending the FileName, Directory 1, and Directory 2 to the View.
+            FileMethods.move_files_output(FileName, dir_a, dir_b)
+
+
+
     def move_files_command(self):
-        try: 
+        try:
             output_padding = " " * (len("Name") - len("Extension"))
-            # Normalize user extensions to lowercase
             normalized_extensions = [ext.lower() for ext in self.ExtList]
-            # Check for files in the source directory
             from_file_ext = [os.path.splitext(file)[-1].lstrip('.').lower() for file in os.listdir(self.dir_a)]
             for ext in normalized_extensions:
                 if ext not in from_file_ext:
-                    ControllerError = Controller(self.dir_a, self.dir_b, self.ExtList)
-
-                    ControllerError.error_extensions()
-                    time.sleep(1)
+                    time.sleep(2)
+                    #Displaying error message for the instance.
+                    self.controller_instances(4)
                     break
             for file in os.listdir(self.dir_a):
-                if any(file.lower().endswith(ext) for ext in normalized_extensions):  # Normalize case
+                if any(file.lower().endswith(ext) for ext in normalized_extensions):
                     src_path = os.path.join(self.dir_a, file)
                     dest_path = os.path.join(self.dir_b, file)
                     shutil.move(src_path, dest_path)
-                # The model should not print anything out so we have to move this back to the controller somehow.
-                FileStatus = Views(self.dir_a, self.dir_b)
-                FileStatus.move_files_output(file,src_path, dest_path)
-            SendBackController = Controller(self.dir_a, self.dir_b, self.ExtList)
-            SendBackController.command_line_options()
-        except:
-            BackToMenu = Controller(self.dir_a, self.dir_b, self.ExtList)
-            BackToMenu.error_exeption()
+                self.view_instances(2,file)
+        except NameError:
+            self.controller_instances(2)
+
+         
 
     def move_folders_command(self):
         try:
             shutil.move(self.dir_a, self.dir_b)
-            MovedFolder = Views(self.dir_a, self.dir_b)
-            MovedFolder.move_folders_output(self.dir_a, self.dir_b)
-        except: 
-            BackToMenu = Controller(self.dir_a, self.dir_b, self.ExtList)
-            BackToMenu.error_exeption()
-
-    
+            self.call_foldermove_method()
+        except NameError:
+            self.controller_instances(3)
+                
     def move_2trash_command(self):
         try:
             send2trash.send2trash(self.dir_a)
         except:
             BackToMenu = Controller(self.dir_a, self.dir_b, self.ExtList)
-            BackToMenu.error_exeption()
+            BackToMenu.relay_error_message()
 
 if __name__ == "__main__":
     dir_a = None
